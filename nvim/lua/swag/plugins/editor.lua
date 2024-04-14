61f312c4c3d5caf9f7772becdb7b283b4530c39b
austingw <senortinman@gmail.com>
2024-04-14 19:58:26 -0400

    Remove unneeded instance of conform

--- nvim/lua/swag/plugins/editor.lua
+++ nvim/lua/swag/plugins/editor.lua
@@ -131,25 +131,6 @@ return {
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
-  },
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
